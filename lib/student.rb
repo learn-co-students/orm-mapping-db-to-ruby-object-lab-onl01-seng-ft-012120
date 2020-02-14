76be5adb5a6f2a6f69b981703890694b74bd3da7
reddevilcero <reddevil_cero@hotmail.com>
2020-02-14 14:06:20 +0000

    Now start to makemore senses

--- lib/student.rb
+++ lib/student.rb
@@ -3,16 +3,33 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    self.new.tap do |ins|
+      ins.id = row[0]
+      ins.name = row[1]
+      ins.grade = row[2]
+    end
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+   
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE name = ? LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql,name).map{|row| self.new_from_db(row)}.first
   end
   
   def save
@@ -40,4 +57,40 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
+  def self.all_students_in_grade_9
+    sql = " SELECT * FROM students WHERE grade = 9"
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.students_below_12th_grade
+    sql = " SELECT * FROM students WHERE grade < 12"
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.all_students_in_grade_X(grade)
+    sql = " SELECT * FROM students WHERE grade = ?"
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.first_X_students_in_grade_10(num)
+    sql = " SELECT * FROM students WHERE grade = 10 LIMIT ?"
+    DB[:conn].execute(sql, num).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.first_student_in_grade_10
+    sql = " SELECT * FROM students WHERE grade = 10 LIMIT 1"
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
 end
