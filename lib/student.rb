44eec509e4134dc02f462663b006e88d37705a16
Kevin Villalobos <kevinv15v@gmail.com>
2020-03-08 20:31:11 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -12,11 +12,13 @@ class Student
   def self.all
     sql = <<-SQL
       SELECT *
-      FROM songs
+      FROM students
     SQL
  
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
   end
+end
 
   def self.find_by_name(name)
      sql = <<-SQL
@@ -56,4 +58,74 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+  
+  def self.all_students_in_grade_9
+    
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 9
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+  
+  def self.students_below_12th_grade
+    
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade < 12
+    SQL
+     DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+  end
+end
+  
+
+  
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT ?
+    SQL
+    DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+    end
+  end
+  
+
+  def self.first_student_in_grade_10
+    
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT 1
+    SQL
+    first_student_row = DB[:conn].execute(sql)[0]
+    self.new_from_db(first_student_row)
+    end
+    
+    
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+    SQL
+    DB[:conn].execute(sql, x)
+  end
+  
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade) 
+      VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+  end
+    
 end
